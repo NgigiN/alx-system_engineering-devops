44ad5fd0bf690f07d0b42157a81a697b69ffad66
NgigiN <ngigi.nyongo@gmail.com>
2023-06-26 18:39:04 +0300

    fixing puppet lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #install a flask from pip3
 exec { 'install flask':
-  command   => 'pip3 install flask',
-  path      => '/usr/bin/',
+  command =>  'pip3 install flask',
+  path    =>  '/usr/bin/',
 }
