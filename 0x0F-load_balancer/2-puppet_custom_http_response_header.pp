171a0a36516c413261396e5aefe355fc74087b84
NgigiN <ngigi.nyongo@gmail.com>
2023-07-03 22:24:11 +0300

    step 1

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,5 @@
+# Script creating a custom HTTP header response with puppet.
+exec { 'configure-nginx':
+  command  => 'apt-get -y update && apt-get -y install nginx && sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default && service nginx restart',
+  provider => shell,
+}
