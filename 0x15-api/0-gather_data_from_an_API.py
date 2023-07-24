4df0538182b77225a04ecd4d226af0542961a127
NgigiN <ngigi.nyongo@gmail.com>
2023-07-24 22:18:12 +0300

    fixing bug with 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -25,10 +25,11 @@ def display():
                 NUMBER_OF_DONE_TASKS += 1
                 TASK_TITLE.append(to_do.get('title'))
 
-        print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
-              NUMBER_OF_DONE_TASKS, TOTAL_NUM_OF_TASKS))
-        for task in TASK_TITLE:
-            print("\t {}".format(task))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUM_OF_TASKS))
+    for task in TASK_TITLE:
+        print("\t {}".format(task))
 
 
 if __name__ == "__main__":
