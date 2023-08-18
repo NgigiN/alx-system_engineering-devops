ed949b190fb5f0b3c0b39e4ffb43849ae9b945dd
NgigiN <ngigi.nyongo@gmail.com>
2023-08-18 10:26:34 +0300

    fixing puppet issues

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # This manuscript raises the number of requests the nginx server handles
 exec { 'fix--for-ngix':
- command => 'sed -i "s/15/4096" /etc/default/nginx',
- path	   => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 #Restart Nginx for changes to apply
 -> exec { 'nginx-restart':
- command => 'nginx restart',
- path    => '/etc/init.d/'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
