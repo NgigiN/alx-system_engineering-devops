9ee66dce9e4df8f1b0840a0cd5ec7b52c0966a70
NgigiN <ngigi.nyongo@gmail.com>
2023-10-10 22:16:50 +0300

    adding file 0-subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Function that queries the Reddit API and returns the number of active
+subscribers. Returns 0 if the function should return 0."""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """ function that queries the Reddit API """
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
