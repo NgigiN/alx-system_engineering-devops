16a1a01465329c57c8fe45e6e56e59d13c5f0f19
NgigiN <ngigi.nyongo@gmail.com>
2023-10-19 20:58:38 +0300

    final fix for file 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,7 +3,7 @@
 # user and open a file without any error message
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:bin/'
+  path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'increase-soft-file-limit-for-holberton-user':
