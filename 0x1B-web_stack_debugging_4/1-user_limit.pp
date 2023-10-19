a1297d6727719dab08e213b91abe9f369533ec32
NgigiN <ngigi.nyongo@gmail.com>
2023-10-19 20:38:19 +0300

    fixing file 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -8,5 +8,5 @@ exec { 'increase-hard-file-limit':
 
 exec { 'increase-soft-file-limit':
   command => 'sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:bin'
+  path    => '/usr/local/bin/:/bin/'
 }
